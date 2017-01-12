d879d9e0a995b884573ef1a59517d36a83598e5c
Ben Hammack <hammack.b@gmail.com>
2017-01-12 11:41:27 -0500

    Added python implementation from remote source

---
+++ a1/pdollar.py
@@ -0,0 +1,8 @@
+# Using sonovice's implementation of the pdollar recognition engine.
+# https://github.com/sonovice/dollarpy
+# Included in this project as "dollarpy.py"
+import sys
+from dollarpy import Recognizer, Template, Point
+
+
+
