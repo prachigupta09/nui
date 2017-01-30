d37f8f4b0ec49bc80be7cc75bded0e81b010a94f
Ben Hammack <hammack.b@gmail.com>
2017-01-30 14:51:39 -0500

    Can now display board svg

--- project/start.py
+++ project/start.py
@@ -21,26 +21,50 @@ def main():
 
 def start(host_addr, port_num):
 	app = web.Application()
+	chessgame = Game()
     #service = Service(args.css.read() if args.css else None)
     #app.router.add_get("/board.png", service.render_png)
     #app.router.add_get("/board.svg", service.render_svg)
-	app.router.add_get("/", handle)
+	app.router.add_get("/", chessgame.get_board)
 	
 	web.run_app(app, port=port_num, host=host_addr)
 
-	
+# Todo: Decouple the GameManager from the Game.
 class Game:
+	
+	# Initialize the game. 
 	def __init__(self):
-		self.css == chess.svg.DEFAULT_STYLE
+		self.board = chess.Board()
+		self.css = chess.svg.DEFAULT_STYLE
 	
+	
+	# Responds to a GET request for the board.
+	# Todo: Allow for parameters to alter the board.svg file?
+	@asyncio.coroutine
 	def get_board(self, request):
-		
+		return web.Response(text=self.render_board(request), 
+			content_type="image/svg+xml")
 	
+	
+	# Responds to a POST request for a move.
+	@asyncio.coroutine
 	def post_move(self, request):
+		# Apply the move. If it is successful, redirect to getting the board.
+		return web.Response()
+		pass
 	
 	
-	
+	# Returns the active board, in svg format.
+	def render_board(self, request):
+		return chess.svg.board(board=self.board, style=self.css)
 
-
+	
 if __name__ == "__main__":
 	main()
+	
+	
+	
+	
+	
+	
+	
