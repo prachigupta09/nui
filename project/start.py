de8be3ef02c7b644879cb95eda5a2c4d1a4ab6c4
Ben Hammack <hammack.b@gmail.com>
2017-01-30 16:34:10 -0500

    Haha ITS WORKING!!!

--- project/start.py
+++ project/start.py
@@ -8,13 +8,12 @@
 #------------------------------------------------------------------------------
 # Libraries:
 # Inspired by https://github.com/niklasf/web-boardimage
+# Reference http://python-chess.readthedocs.io/en/v0.16.2/core.html
 from aiohttp import web
 import asyncio
 import chess
 import chess.svg
-
-
-
+#------------------------------------------------------------------------------
 
 def main():
 	start("127.0.0.1", 7777)
@@ -26,6 +25,7 @@ def start(host_addr, port_num):
     #app.router.add_get("/board.png", service.render_png)
     #app.router.add_get("/board.svg", service.render_svg)
 	app.router.add_get("/", chessgame.get_board)
+	app.router.add_post("/", chessgame.post_move)
 	
 	web.run_app(app, port=port_num, host=host_addr)
 
@@ -49,15 +49,53 @@ class Game:
 	# Responds to a POST request for a move.
 	@asyncio.coroutine
 	def post_move(self, request):
+		data = yield from request.post()
+		#print(data)
 		# Apply the move. If it is successful, redirect to getting the board.
-		return web.Response()
-		pass
+		rv = self.make_move(data)
+		return web.Response(status=rv, text=self.render_board(request), 
+			content_type="image/svg+xml")
 	
 	
 	# Returns the active board, in svg format.
 	def render_board(self, request):
 		return chess.svg.board(board=self.board, style=self.css)
+		
+		
+	# Applies a post request move to the board. Returns status code if move invalid.
+	def make_move(self, post_data):
+		# If the post_data looks weird, it's because it's url encoded. 	
 
+		# First, validate the paramters we need to make a move.
+		try:
+			piece = post_data["piece"]
+		except (KeyError, ValueError):
+			piece = ""
+		try:
+			from_square = post_data["from"]
+		except (KeyError, ValueError):
+			from_square = ""	
+		try:
+			to_square = post_data["to"]
+		except (KeyError, ValueError):
+			raise web.HTTPBadRequest(reason="Parameter [to] not specified!")
+		
+		# Attempt to make the move.
+		move_str = piece + from_square + to_square
+		try:
+			move = self.board.parse_san(move_str)
+		except (ValueError):
+			# Invalid move specified.
+			raise web.HTTPBadRequest(reason="Move: " + move_str + " is invalid!")
+			
+		if move in self.board.legal_moves:
+			# Apply the legal move.
+			self.board.push(move)
+			return 202
+		else:
+			raise web.HTTPBadRequest(reason="Move: " + move_str + " is illegal.")
+			
+			
 	
 if __name__ == "__main__":
 	main()
